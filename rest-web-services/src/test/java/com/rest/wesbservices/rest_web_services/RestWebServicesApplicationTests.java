106a183c2841149065cd5dbc58d968fd41752512
IsratErin <israterin11@gmail.com>
2026-01-15 21:24:18 +0100

    first hello world REST API using Spring

---
+++ rest-web-services/src/test/java/com/rest/wesbservices/rest_web_services/RestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.wesbservices.rest_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
