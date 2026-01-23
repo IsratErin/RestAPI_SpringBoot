a502e0c87a153d1b14de837dbc2a4c31c836cfe6
IsratErin <israterin11@gmail.com>
2026-01-23 21:01:43 +0100

    enhanced post method for correct http status code and location URI

--- rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/UserDaoService.java
+++ rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/UserDaoService.java
@@ -12,12 +12,14 @@ public class UserDaoService {
 
 	private static List<User> users = new ArrayList<>();
 
+	private static int userId = 0;
+
 	static {
-		users.add(new User(1, "Israt", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Alex", LocalDate.now().minusYears(20)));
-		users.add(new User(3, "Sophia", LocalDate.now().minusYears(10)));
-		users.add(new User(4, "Tilda", LocalDate.now().minusYears(15)));
-		users.add(new User(5, "Erik", LocalDate.now().minusYears(35)));
+		users.add(new User(++userId, "Israt", LocalDate.now().minusYears(30)));
+		users.add(new User(++userId, "Alex", LocalDate.now().minusYears(20)));
+		users.add(new User(++userId, "Sophia", LocalDate.now().minusYears(10)));
+		users.add(new User(++userId, "Tilda", LocalDate.now().minusYears(15)));
+		users.add(new User(++userId, "Erik", LocalDate.now().minusYears(35)));
 	}
 
 	public List<User> findAll() {
@@ -27,6 +29,12 @@ public class UserDaoService {
 	public User retrieveUserInfo(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();
+	}
+
+	public User createNewUser(User user) {
+		user.setId(++userId);
+		users.add(user);
+		return user;
 	}
 
 }
--- rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/UserResource.java
+++ rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/UserResource.java
@@ -1,10 +1,15 @@
 package com.rest.wesbservices.rest_web_services.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -23,6 +28,14 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveUserInfo(@PathVariable int id) {
 		return service.retrieveUserInfo(id);
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User createdUser = service.createNewUser(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(createdUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
