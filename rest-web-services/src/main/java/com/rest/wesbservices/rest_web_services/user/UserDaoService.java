aa8d2219853379069c515dd588fbdb38a9e6fe67
IsratErin <israterin11@gmail.com>
2026-01-23 09:18:24 +0100

    Get specific use info /users/{id} by using id

--- rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/UserDaoService.java
+++ rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.rest.wesbservices.rest_web_services.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -13,14 +14,19 @@ public class UserDaoService {
 
 	static {
 		users.add(new User(1, "Israt", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Alex", LocalDate.now().minusYears(30)));
-		users.add(new User(3, "Sophia", LocalDate.now().minusYears(30)));
-		users.add(new User(4, "Tilda", LocalDate.now().minusYears(30)));
-		users.add(new User(5, "Erik", LocalDate.now().minusYears(30)));
+		users.add(new User(2, "Alex", LocalDate.now().minusYears(20)));
+		users.add(new User(3, "Sophia", LocalDate.now().minusYears(10)));
+		users.add(new User(4, "Tilda", LocalDate.now().minusYears(15)));
+		users.add(new User(5, "Erik", LocalDate.now().minusYears(35)));
 	}
 
 	public List<User> findAll() {
 		return users;
+	}
+
+	public User retrieveUserInfo(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().get();
 	}
 
 }
