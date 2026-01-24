5c36313a9fe8faab01044dd438289970d5a5f299
IsratErin <israterin11@gmail.com>
2026-01-24 21:54:23 +0100

    Static filtering of API response implemented using @JsonIgnore

---
+++ rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/filtering/FilteringController.java
@@ -0,0 +1,22 @@
+package com.rest.wesbservices.rest_web_services.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public SomeBean doFiltering() {
+		return new SomeBean("1", "2", "3", "4");
+	}
+
+	@GetMapping("/filtering-list")
+	public List<SomeBean> doFilteringList() {
+
+		return Arrays.asList(new SomeBean("1", "2", "3", "4"), new SomeBean("5", "6", "7", "8"));
+	}
+}
