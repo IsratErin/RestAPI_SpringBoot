8aad0c0a66b36fd62fa667b57705cb8c5afc8d00
IsratErin <israterin11@gmail.com>
2026-01-26 10:38:13 +0100

    Created Post Entity with Many to One relationship with User Entity

--- rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/User.java
+++ rest-web-services/src/main/java/com/rest/wesbservices/rest_web_services/user/User.java
@@ -1,10 +1,14 @@
 package com.rest.wesbservices.rest_web_services.user;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -24,6 +28,10 @@ public class User {
 
 	@Past(message = "Birth should be a past date")
 	private LocalDate birthdate;
+
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 
 	public User(Integer id, String name, LocalDate birthdate) {
 		super();
--- rest-web-services/src/main/resources/data.sql
+++ rest-web-services/src/main/resources/data.sql
@@ -3,3 +3,12 @@ values(101,current_date(),'Ranga');
 
 insert into user_details(id,birthdate,name)
 values(102,current_date(),'Raisa');
+
+insert into post(id,description,user_id)
+values(2001,'I want to learn AWS', 101);
+
+insert into post(id,description,user_id)
+values(2002,'I want to learn Microservices', 101);
+
+insert into post(id,description,user_id)
+values(2003,'I want to learn Docker', 102);
